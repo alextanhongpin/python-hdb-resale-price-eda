66cbe05b9f95f155e107df90d57de503285ca15e
Alex Tan Hong Pin <alextan220990@gmail.com>
2026-08-17 09:26:00 +0800

    chore: simplify notebook

--- Makefile
+++ Makefile
@@ -1,2 +1,15 @@
+marimo := uv run marimo
+name := main.py
 run:
-	uv run marimo edit
+	$(marimo) edit
+
+publish:
+	$(marimo) export html-wasm $(name) -o dist --mode run
+
+serve:
+	open http://localhost:3000
+	uv run python -m http.server 3000
+
+lint:
+	uv format
+	$(marimo) check --fix *.py
--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from python-hdb-resale-price-eda!")
+import marimo
+
+__generated_with = "0.23.16"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import polars as pl
+    import pyarrow
+
+    return (pl,)
+
+
+@app.cell
+def _(pl):
+    df = pl.read_csv(
+        "data/Resale flat prices based on registration date from Jan-2017 onwards.csv",
+        infer_schema_length=10000,
+    )
+    return (df,)
+
+
+@app.cell
+def _(df, pl):
+    df_with_date = df.with_columns(
+        pl.col("month").str.to_date(format="%Y-%m").alias("transacted_date"),
+        (pl.col("floor_area_sqm") * 10.7639).round(0).alias("floor_area_sqft"),
+    ).select(pl.exclude(["month"]))
+    df_with_date.select(sorted(df_with_date.columns, reverse=False))
+    return
 
 
 if __name__ == "__main__":
-    main()
+    app.run()
